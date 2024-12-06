24ebf079054f71f11f6d823bc2ddbd22a3b9b069
Aniket Shewale <69764353+AniketShewale08@users.noreply.github.com>
2024-12-06 20:57:03 +0530

    Update views.py

--- employees/views.py
+++ employees/views.py
@@ -1,8 +1,13 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated, AllowAny, BasePermission
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Employee
 from .serializers import EmployeeSerializer
 import django_filters
+from django.contrib.auth.models import User
 
 class EmployeeFilter(django_filters.FilterSet):
     department = django_filters.CharFilter(field_name='department', lookup_expr='exact')
@@ -10,13 +15,14 @@ class EmployeeFilter(django_filters.FilterSet):
 
     class Meta:
         model = Employee
-        fields = ['department', 'role'] 
-
-
-from django_filters.rest_framework import DjangoFilterBackend
+        fields = ['department', 'role']
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-    filter_backends = [DjangoFilterBackend]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = EmployeeFilter
+    search_fields = ['name', 'email']
+    ordering_fields = ['date_joined', 'name']
+    ordering = ['date_joined']
